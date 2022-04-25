8ab07bd090c7ab29b90d93bec84197a3f0df435d
clayton7000 <80071108+clayton7000@users.noreply.github.com>
2022-04-25 10:53:04 -0300

    Commit do projeto

---
+++ src/main/java/com/hello/hello/HelloApplication.java
@@ -0,0 +1,21 @@
+package com.hello.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+        @GetMapping("/")
+        public String Hello(){
+            return "Hello World!";
+        }
+
+}
